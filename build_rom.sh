98e0ee139cf81aab8e5c158d653601930cb85fa3
ShazuKhan6 <84267480+ShazuKhan6@users.noreply.github.com>
2021-05-16 14:38:22 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init -u https://github.com/Corvus-ROM/android_manifest.git -b 11 -g default
 git clone https://github.com/GangsterPlayz1/local_manifests.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom
+# build rom 1
 source build/envsetup.sh
 lunch corvus_RMX1801-userdebug
 make corvus
