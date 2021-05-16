094f245605ddd7d6874ee65471fa6c254424974e
ShazuKhan6 <84267480+ShazuKhan6@users.noreply.github.com>
2021-05-16 16:13:14 +0530

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,6 +1,7 @@
 # sync rom 1
 #trigger build
-repo init -u https://github.com/Corvus-ROM/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
+repo init -u https://github.com/StagOS/manifest.git -b r11
+ -g default,-device,-mips,-darwin,-notdefault
 git clone https://github.com/GangsterPlayz1/local_manifests.git --depth 1 -b aex .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
